ddae369d35ee88dc664e1a21ba64ac80f053fab0
jackie <jackie.lee@neulion.com.cn>
2018-03-18 20:27:36 +0800

    Initial Commit

---
+++ TestAPi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestAPi
+//
+//  Created by jackie on 2018/3/18.
+//  Copyright © 2018年 jackie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
